1db257035c3ae6f11c21074c1bd136f9d096067c
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-06-13 16:17:06 +0200

    vstack

---
+++ NEROV2/Core/Authentication /LoginView .swift
@@ -0,0 +1,20 @@
+//
+//  LoginView .swift
+//  NEROV2
+//
+//  Created by DA M1 MAC 89 on 2023/06/13.
+//
+
+import SwiftUI
+
+struct LoginView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView__Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView_()
+    }
+}
