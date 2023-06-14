f8ebf55e65cbee74079761eb2f0e75f5ed2d9795
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-06-14 10:48:40 +0200

    adding texts field using options N

---
+++ NEROV2/Component/InputView.swift
@@ -0,0 +1,20 @@
+//
+//  InputView.swift
+//  NEROV2
+//
+//  Created by DA M1 MAC 89 on 2023/06/14.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
